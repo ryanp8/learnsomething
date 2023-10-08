657049a21f97bd039c314dee9034eaf89255f399
Ryan Poon <rypoon18@gmail.com>
2023-10-08 15:12:21 -0500

    step 1

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,14 @@
 #include "pricingutil.h"
+
+PricingUtil::PricingUtil() {};
+
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
+    float theoreticalVal = prevPrice * (0.9 + interest) * oleoConstant;
+    this->val = theoreticalVal;
+    return theoreticalVal;
+};
+
+float PricingUtil::getVal() {
+    return this->val;
+}
